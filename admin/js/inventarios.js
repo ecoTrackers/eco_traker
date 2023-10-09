5a1f9ae2766e56523dc5858300b352e48e28f4a3
DiegoSSanabria <diegossanabria@outlook.com>
2023-10-09 13:24:27 -0500

    arreglo de local store para los productos agregados

--- admin/js/inventarios.js
+++ admin/js/inventarios.js
@@ -1,4 +1,4 @@
-function cargarCategoriasInventario(showAsTitles) {
+async function cargarCategoriasInventario(showAsTitles) {
     const categoryList = document.getElementById('categoryList');
 
     if (categoryList) {
@@ -6,7 +6,7 @@ function cargarCategoriasInventario(showAsTitles) {
 
         categoryList.innerHTML = '';
 
-        categories.forEach(categoryName => {
+        for (const categoryName of categories) {
             const categoryContainer = document.createElement('div');
             categoryContainer.className = showAsTitles ? 'category-container' : 'list-group-item d-flex flex-column mb-4';
 
@@ -16,7 +16,15 @@ function cargarCategoriasInventario(showAsTitles) {
 
             const productList = document.createElement('ul');
             productList.className = 'list-group mt-2';
-            productList.id = `${categoryName.replace(' ', '')}-products`; 
+            productList.id = `${categoryName.replace(/\s+/g, '-')}-products`;
+
+            // localstore
+            const productsFromLocalStorage = JSON.parse(localStorage.getItem(categoryName)) || [];
+
+            for (const product of productsFromLocalStorage) {
+                const productItem = createProductElement(product); 
+                productList.appendChild(productItem);
+            }
 
             const addButton = document.createElement('button');
             addButton.className = 'btn btn-primary';
@@ -30,7 +38,7 @@ function cargarCategoriasInventario(showAsTitles) {
             categoryContainer.appendChild(productList);
 
             categoryList.appendChild(categoryContainer);
-        });
+        }
     } else {
         console.error('Element with id "categoryList" not found.');
     }
@@ -44,13 +52,13 @@ function agregarProducto(categoryName) {
         return;
     }
 
-    let productQuantity = 0;
+    let productQuantity;
 
     while (true) {
         const quantityInput = prompt(`Ingrese la cantidad del producto "${productName}":`);
 
         if (quantityInput === null) {
-            return;  // Cancelado por el usuario
+            return;
         }
 
         productQuantity = parseInt(quantityInput);
@@ -62,6 +70,37 @@ function agregarProducto(categoryName) {
         }
     }
 
+    // Crear un objeto de producto
+    const product = {
+        name: productName,
+        quantity: productQuantity
+    };
+
+    // Crear el elemento del producto utilizando createProductElement
+    const productItem = createProductElement(product);
+
+    // Encuentra la lista de productos para la categoría correspondiente
+    const productListId = `${categoryName.replace(/\s+/g, '-')}-products`;
+    const productList = document.getElementById(productListId);
+
+    if (!productList) {
+        console.error('Product list not found for category:', categoryName);
+        return;
+    }
+
+    productList.appendChild(productItem);
+
+    // Obtiene los productos actuales de la categoría o inicializa un array vacío
+    const products = JSON.parse(localStorage.getItem(categoryName)) || [];
+
+    // Agrega el nuevo producto a la lista de productos
+    products.push(product);
+
+    // Guarda la lista de productos actualizada en el localStorage
+    localStorage.setItem(categoryName, JSON.stringify(products));
+}
+
+function createProductElement(product) {
     const productItem = document.createElement('li');
     productItem.className = 'list-group-item d-flex justify-content-between align-items-center';
 
@@ -73,26 +112,27 @@ function agregarProducto(categoryName) {
 
     const productNameElement = document.createElement('span');
     productNameElement.className = 'product-name me-auto';
-    productNameElement.textContent = productName;
+    productNameElement.textContent = product.name; // Usar el nombre del producto del objeto
 
     const decrementButton = document.createElement('button');
     decrementButton.className = 'btn btn-outline-secondary btn-sm';
     decrementButton.innerHTML = '<i class="fa-solid fa-minus"></i>';
     decrementButton.addEventListener('click', () => {
-        decrementProductQuantity(productQuantityElement);
+        decrementProductQuantity(productQuantityElement, product.name);
     });
+    
 
     const productQuantityElement = document.createElement('span');
     productQuantityElement.className = 'product-quantity-badge badge bg-secondary';
-    productQuantityElement.textContent = productQuantity;
+    productQuantityElement.textContent = product.quantity; // Usar la cantidad del producto del objeto
 
     const incrementButton = document.createElement('button');
     incrementButton.className = 'btn btn-outline-secondary btn-sm';
     incrementButton.innerHTML = '<i class="fa-solid fa-plus"></i>';
     incrementButton.addEventListener('click', () => {
-        incrementProductQuantity(productQuantityElement);
+        incrementProductQuantity(productQuantityElement, product.name);
     });
-
+    
     const buttonsContainer = document.createElement('div');
     buttonsContainer.className = 'quantity-buttons pe-2';
     buttonsContainer.appendChild(decrementButton);
@@ -105,14 +145,14 @@ function agregarProducto(categoryName) {
     editButton.className = 'btn btn-outline-success btn-sm mx-1';
     editButton.innerHTML = '<i class="fa-solid fa-pencil"></i>';
     editButton.addEventListener('click', () => {
-        editarNombreProducto(productItem);
+        editarNombreProducto(productItem, product.name);
     });
 
     const deleteButton = document.createElement('button');
-    deleteButton.className = 'btn btn-outline-danger btn-sm ';
+    deleteButton.className = 'btn btn-outline-danger btn-sm';
     deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
     deleteButton.addEventListener('click', () => {
-        eliminarProducto(productItem);
+        eliminarProducto(productItem, product.name);
     });
 
     productContent.appendChild(productNameElement);
@@ -122,46 +162,93 @@ function agregarProducto(categoryName) {
 
     productItem.appendChild(productContent);
 
-    // Encuentra la lista de productos para la categoría correspondiente
-    const productList = document.getElementById(`${categoryName.replace(/\s+/g, '-')}-products`);
-    if (productList) {
-        // Insertar antes del botón de agregar
-        const addButton = productList.nextElementSibling; // Obtener el botón de agregar
-        productList.insertBefore(productItem, addButton);
-    } else {
-        console.error('Product list not found for category:', categoryName);
-    }
+    return productItem;
 }
 
-function editarNombreProducto(productItem) {
-    const newProductName = prompt('Editar nombre del producto:', productItem.querySelector('.product-name').textContent);
+function editarNombreProducto(productItem, oldProductName) {
+    const newProductName = prompt('Editar nombre del producto:', oldProductName);
     if (newProductName !== null && newProductName.trim() !== '') {
-        productItem.querySelector('.product-name').textContent = newProductName;
+        const productNameElement = productItem.querySelector('.product-name');
+        productNameElement.textContent = newProductName;
+
+        // Actualizar el nombre del producto en el localStorage
+        actualizarProductoEnLocalStorage(oldProductName, newProductName);
+    } else if (newProductName.trim() === '') {
+        alert('El nombre del producto no puede estar vacío.');
     }
 }
 
+//Pendiente arreglar funcionalidad
+function actualizarProductoEnLocalStorage(categoryName, oldProductName, newProductName) {
+    // Obtener productos actuales de la categoría
+    const products = JSON.parse(localStorage.getItem(categoryName)) || [];
 
-function editarNombreProducto(productItem) {
-    const newProductName = prompt('Editar nombre del producto:', productItem.querySelector('.product-name').textContent);
-    if (newProductName !== null && newProductName.trim() !== '') {
-        productItem.querySelector('.product-name').textContent = newProductName;
-    }
+    // Encontrar el producto con el nombre antiguo y actualizarlo
+    const updatedProducts = products.map(product => {
+        if (product.name === oldProductName) {
+            return {
+                name: newProductName,
+                quantity: product.quantity
+            };
+        }
+        return product;
+    });
+
+    // Guardar la lista actualizada en el localStorage
+    localStorage.setItem(categoryName, JSON.stringify(updatedProducts));
 }
 
-function incrementProductQuantity(quantityElement) {
+
+function incrementProductQuantity(quantityElement, productName) {
     let quantity = parseInt(quantityElement.textContent);
     quantity++;
     quantityElement.textContent = quantity;
+    actualizarCantidadEnLocalStorage(productName, categoryName, quantity);
 }
 
-function decrementProductQuantity(quantityElement) {
+function decrementProductQuantity(quantityElement, productName) {
     let quantity = parseInt(quantityElement.textContent);
     if (quantity > 0) {
         quantity--;
         quantityElement.textContent = quantity;
+        actualizarCantidadEnLocalStorage(productName, categoryName, quantity);
+    }
+}
+//pendiente arreglar funcionalidad
+function actualizarCantidadEnLocalStorage(productName, categoryName, newQuantity) {
+    const products = JSON.parse(localStorage.getItem(categoryName)) || [];
+
+    const updatedProducts = products.map(product => {
+        if (product.name === productName) {
+            return {
+                name: productName,
+                quantity: newQuantity
+            };
+        }
+        return product;
+    });
+
+    localStorage.setItem(categoryName, JSON.stringify(updatedProducts));
+}
+
+function eliminarProducto(productItem, productName) {
+    if (confirm(`¿Estás seguro de que quieres eliminar el producto "${productName}"?`)) {
+        // Eliminar el producto del localStorage
+        eliminarProductoDeLocalStorage(productName);
+
+        // Eliminar el elemento del producto del DOM
+        productItem.remove();
     }
 }
 
-function eliminarProducto(productItem) {
-    productItem.remove();
+function eliminarProductoDeLocalStorage(categoryName, productName) {
+    const products = JSON.parse(localStorage.getItem(categoryName)) || [];
+
+    // Filtra los productos, excluyendo el que se va a eliminar
+    const updatedProducts = products.filter(product => product !== productName);
+
+    // Actualiza la lista de productos en el localStorage
+    localStorage.setItem(categoryName, JSON.stringify(updatedProducts));
 }
+
+
